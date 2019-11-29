1e9904b64b023aab439414e57c25830b53ef969f
Benjamin Böhmke <benjamin@boehmke.net>
2019-11-29 22:12:38 +0100

    added system config and actions

---
+++ system.go
@@ -0,0 +1,84 @@
+package alpine_builder
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/GehirnInc/crypt"
+	_ "github.com/GehirnInc/crypt/sha256_crypt"
+)
+
+// system configurations
+var systemShadow = "/data/etc/shadow"
+var systemDropbearConfig = "/data/etc/dropbear/dropbear.conf"
+var systemDropbearRestart = "rc-service dropbear start"
+var systemShutdown = "poweroff"
+var systemReboot = "reboot"
+
+// SystemSetRootPassword update shadow file
+func SystemSetRootPassword(password string) error {
+	crypter := crypt.SHA256.New()
+
+	// generate password line
+	hash, err := crypter.Generate([]byte(password), nil)
+	if err != nil {
+		return fmt.Errorf("failed to generate hash: %w", err)
+	}
+	line := fmt.Sprintf("root:%s:0:0:::::", hash)
+
+	// write shadow file
+	return ioutil.WriteFile(systemShadow, []byte(line), os.ModePerm)
+}
+
+// SystemEnableSSH server
+func SystemEnableSSH() error {
+	err := ioutil.WriteFile(systemDropbearConfig, []byte("DROPBEAR_OPTS=\"\""), os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to write ssh config: %w", err)
+	}
+
+	cmdSplit := strings.Split(systemDropbearRestart, " ")
+	cmd := exec.Command(cmdSplit[0], cmdSplit[1:]...)
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to restart ssh server: %w", err)
+	}
+	return nil
+}
+
+// SystemDisableSSH server
+func SystemDisableSSH() error {
+	err := ioutil.WriteFile(systemDropbearConfig, []byte("DROPBEAR_OPTS=\"-p 127.0.0.1:22\""), os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to write ssh config: %w", err)
+	}
+
+	cmdSplit := strings.Split(systemDropbearRestart, " ")
+	cmd := exec.Command(cmdSplit[0], cmdSplit[1:]...)
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to restart ssh server: %w", err)
+	}
+	return nil
+}
+
+func SystemShutdown() error {
+	cmd := exec.Command(systemShutdown)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to start system shutdown: %w", err)
+	}
+	return nil
+}
+
+func SystemReboot() error {
+	cmd := exec.Command(systemReboot)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to start system reboot: %w", err)
+	}
+	return nil
+}
---
+++ system_test.go
@@ -0,0 +1,75 @@
+package alpine_builder
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/GehirnInc/crypt"
+	_ "github.com/GehirnInc/crypt/sha256_crypt"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	systemShadow = "test_shadow"
+	systemDropbearConfig = "test_dropbear"
+	systemDropbearRestart = "restart_dropbear_command"
+	systemReboot = "reboot_command"
+	systemShutdown = "shutdown_command"
+}
+
+func TestSystemSetRootPassword(t *testing.T) {
+	ass := assert.New(t)
+
+	ass.NoError(SystemSetRootPassword("password"))
+
+	data, err := ioutil.ReadFile(systemShadow)
+	ass.NoError(err)
+	hash := strings.Split(string(data), ":")[1]
+
+	crypter := crypt.SHA256.New()
+	ass.NoError(crypter.Verify(hash, []byte("password")))
+
+	_ = os.Remove(systemShadow)
+}
+
+func TestSystemEnableSSH(t *testing.T) {
+	ass := assert.New(t)
+
+	ass.EqualError(SystemEnableSSH(), "failed to restart ssh server: exec: \"restart_dropbear_command\": executable file not found in $PATH")
+
+	data, err := ioutil.ReadFile(systemDropbearConfig)
+	ass.NoError(err)
+
+	ass.Equal("DROPBEAR_OPTS=\"\"", string(data))
+
+	_ = os.Remove(systemDropbearConfig)
+}
+
+func TestSystemDisableSSH(t *testing.T) {
+	ass := assert.New(t)
+
+	ass.EqualError(SystemDisableSSH(), "failed to restart ssh server: exec: \"restart_dropbear_command\": executable file not found in $PATH")
+
+	data, err := ioutil.ReadFile(systemDropbearConfig)
+	ass.NoError(err)
+
+	ass.Equal("DROPBEAR_OPTS=\"-p 127.0.0.1:22\"", string(data))
+
+	_ = os.Remove(systemDropbearConfig)
+}
+
+func TestSystemReboot(t *testing.T) {
+	ass := assert.New(t)
+
+	ass.EqualError(SystemReboot(),
+		"failed to start system reboot: exec: \"reboot_command\": executable file not found in $PATH")
+}
+
+func TestSystemShutdown(t *testing.T) {
+	ass := assert.New(t)
+
+	ass.EqualError(SystemShutdown(),
+		"failed to start system shutdown: exec: \"shutdown_command\": executable file not found in $PATH")
+}
