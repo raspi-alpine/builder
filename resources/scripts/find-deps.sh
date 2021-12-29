7594801dabacc616935109e56e94aaae084744e2
Duncan Bellamy <dunk@denkimushi.com>
2021-12-29 21:11:47 +0000

    Find dependencies for modules
    * add ADDITIONAL_KERNEL_MODULES and ADDITIONAL_DIR_KERNEL_MODULES envirnoment variables
    * fixes #15

---
+++ resources/scripts/find-deps.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# check it is not a directory or empty
+[ -z "$1" ] && echo "No module to find deps for" && exit 1
+echo "$1" | grep -q "\.ko" || exit 0
+
+SAVE="/tmp/modules.save"
+MOD="$(echo "$1" | sed "s/.\///")"
+echo "  > $MOD"
+grep "^$MOD" ./modules.dep | sed "s/://" >> "$SAVE"
