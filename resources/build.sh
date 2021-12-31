1201a0c6eb68103db1f70f19351f286a028d8770
Duncan Bellamy <dunk@denkimushi.com>
2021-12-31 11:17:13 +0000

    Use resize2fs twice instead of truncate to shrink filesystem

--- resources/build.sh
+++ resources/build.sh
@@ -442,10 +442,9 @@ case "$SIZE_ROOT_FS" in
     m4 -D xFS=ext4 -D xIMAGE=rootfs.xFS -D xLABEL="rootfs" -D xSIZE="$SIZE_ROOT_PART" -D xFEATURES="extents,dir_index" -D xEXTRAARGS="-m 0" \
       -D xUSEMKE2FS "$RES_PATH"/m4/genimage.m4 > "$WORK_PATH"/genimage_root.cfg
     make_image ${ROOTFS_PATH} ${WORK_PATH}/genimage_root.cfg
-    resize2fs -M ${IMAGE_PATH}/rootfs.ext4
-    SIZE="$(dumpe2fs -h ${IMAGE_PATH}/rootfs.ext4 | awk -F: '/Block count/{count=$2} /Block size/{size=$2} END{print count*size}')"
-    truncate -s "$SIZE" ${IMAGE_PATH}/rootfs.ext4
-    colour_echo "Shrunk rootfs to $SIZE bytes" ;;
+    resize2fs -fM ${IMAGE_PATH}/rootfs.ext4
+    resize2fs -fM ${IMAGE_PATH}/rootfs.ext4
+    colour_echo "Shrunk rootfs to $(du -h ${IMAGE_PATH}/rootfs.ext4 | cut -f1)" ;;
   *)
     colour_echo 'Will not shrink rootfs'
     m4 -D xFS=ext4 -D xIMAGE=rootfs.xFS -D xLABEL="rootfs" -D xSIZE="$SIZE_ROOT_FS" -D xUSEMKE2FS \
