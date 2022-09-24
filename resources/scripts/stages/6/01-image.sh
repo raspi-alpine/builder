e01cca854d665c535a3ed68a8ae6c43a8120c160
Duncan Bellamy <dunk@denkimushi.com>
2022-09-24 20:41:55 +0100

    move finished echo inside if for stages/6/01-image.sh

--- resources/scripts/stages/6/01-image.sh
+++ resources/scripts/stages/6/01-image.sh
@@ -3,5 +3,5 @@
 if [ -f ${INPUT_PATH}/${CUSTOM_IMAGE_SCRIPT} ]; then
   # shellcheck source=/dev/null
   . ${INPUT_PATH}/${CUSTOM_IMAGE_SCRIPT}
+  colour_echo "   Finished running user image.sh script" "$Blue"
 fi
-colour_echo "   Finished running user image.sh script" "$Blue"
