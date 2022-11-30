b92c8e820a37ecd25da82144c0c03d7ab2178c39
Duncan Bellamy <dunk@denkimushi.com>
2022-11-30 21:28:13 +0000

    invoke make directly with ab_cache for megaind
    add user to dialout group as used for usb serial
    use mdev.conf to set i2c permissions to root:dialout

--- examples/node-red/build_image.sh
+++ examples/node-red/build_image.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-docker run --pull always --rm -it -v "$PWD"/input:/input -v "$PWD"/output:/output --env-file megaind.env ghcr.io/raspi-alpine/builder
+# make sure have latest image version if not passed as variable
+# first try github
+if [ -z "$DOCKIMAGE" ]; then
+  DOCKIMAGE=ghcr.io/raspi-alpine/builder
+  if ! docker image pull "$DOCKIMAGE"; then
+    # fallback to gitlab if github fails
+    DOCKIMAGE=registry.gitlab.com/raspi-alpine/builder/master
+    docker image pull "$DOCKIMAGE"
+  fi
+fi
+docker run --rm -it -v "$PWD"/input:/input -v "$PWD"/output:/output --env-file megaind.env "$DOCKIMAGE"
