9a833b20742feffbc01f1541bef32e0627277764
Duncan Bellamy <dunk@denkimushi.com>
2025-10-25 12:06:55 +0100

    fix node-red example building

--- examples/node-red/input/cache-scripts/install-node-red.sh
+++ examples/node-red/input/cache-scripts/install-node-red.sh
@@ -3,8 +3,8 @@
 # npm writes to root during install
 mv /root /root.orig
 npm config set foreground-scripts=true
-npm install -g node-red node-red-dashboard \
-  node-red-contrib-ui-led node-red-contrib-chronos node-red-contrib-moment node-red-contrib-modbus
+npm install -g node-red @flowfuse/node-red-dashboard \
+  @flowfuse/node-red-dashboard-2-ui-led node-red-contrib-chronos node-red-contrib-moment node-red-contrib-modbus
 
 # sequent microstsyems node red
 npm install -g node-red-contrib-sm-ind node-red-contrib-sm-16inputs
