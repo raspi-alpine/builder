53e06186b89f9febfbf1ca77e2bdb52831e9cab9
Duncan Bellamy <dunk@denkimushi.com>
2026-06-08 15:09:26 +0100

    Display countdown instead of instant shutdown

--- tests/simple-image/hello.sh
+++ tests/simple-image/hello.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 echo "hello world!"
+# Countdown for one minute before poweroff
+i=30
+while [ $i -gt 0 ]; do
+  printf "\rPowering off in %2d seconds..." "$i"
+  sleep 1
+  i=$((i - 1))
+done
+echo ""
 poweroff
